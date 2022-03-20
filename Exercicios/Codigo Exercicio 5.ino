2d7a87d87df3a40bf7b7e9eb3cb7f89525a46d48
amandavaitcunas <79206780+amandavaitcunas@users.noreply.github.com>
2022-03-19 21:54:46 -0300

    Create Codigo Exercicio 5.ino

---
+++ Exercicios/Codigo Exercicio 5.ino
@@ -0,0 +1,34 @@
+int LED = 2;
+int BOTAO = 3; 
+int ESTADO_BOTAO = 0; 
+int VAR = 0;
+int VAR2 = 0;
+
+void setup()
+
+{
+ pinMode(LED, OUTPUT); 
+ pinMode(BOTAO, INPUT); 
+}
+
+void loop()
+
+{
+ VAR = digitalRead(BOTAO); 
+ 
+ if (VAR == 1 && VAR2 == 0) 
+ {
+ ESTADO_BOTAO = 1- ESTADO_BOTAO;
+   delay(20);
+ }
+ VAR2=VAR;
+  if(ESTADO_BOTAO == 0){
+    
+ digitalWrite(LED, 0);
+
+ }
+ else 
+ {
+ digitalWrite(LED, 1); 
+ }
+}
